60009e7703b34c037d573ec8938ef144e1bf1199
Elizabeth Paul <elizapaul01@gmail.com>
2019-04-15 00:22:16 +0200

    Updates

---
+++ precache-manifest.2c00344f1a8d43fde82868e183542cff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ade3880c2edd6984b48",
+    "url": "/MarkDown/static/css/main.6c26b309.chunk.css"
+  },
+  {
+    "revision": "3ade3880c2edd6984b48",
+    "url": "/MarkDown/static/js/main.0255de1d.chunk.js"
+  },
+  {
+    "revision": "e8201e780727f58e5a4e",
+    "url": "/MarkDown/static/js/runtime~main.43083302.js"
+  },
+  {
+    "revision": "05bf06212dffc7bed76a",
+    "url": "/MarkDown/static/js/2.276159d4.chunk.js"
+  },
+  {
+    "revision": "44b6720db25fde1e0da01c4061397bdd",
+    "url": "/MarkDown/index.html"
+  }
+];
